7b77838b19790678f086cbd4e4cdaf38e4d4715a
gade shiva kumar <shivakumargade955@gmail.com>
2025-06-04 10:33:31 +0530

    logs4

--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -11,7 +11,7 @@ export default function Private({children}) {
     })
     .then((res)=>{
         if(res.ok){
-          console.log(res.json())
+          res.json().then((data)=>console.log(data))
         setlock(true)
         }
         else{
